65369596847992ce81d15420da78790f94038e1d
ilyakh <ilyakh@ulrik.uio.no>
2013-06-28 04:32:57 +0200

    introduces the communication with the motion processing unit

--- logger_MPU9150.ino
+++ logger_MPU9150.ino
@@ -2,7 +2,9 @@
 #include "RTClib.h"
 
 #include "I2Cdev.h"
-#include "MPU6050.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+//#include "MPU6050.h" // not necessary if using MotionApps include file
 
 #include "SD.h"
 
@@ -25,6 +27,64 @@ const int LOOP_DELAY = 30; // the i2c communication with the clock chip can be d
 // ... continuing to the next loop.
 
 
+////////////////////////////////////////////////
+///    DMP    /////////////////////////////////
+//////////////////////////////////////////////
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+
+// uncomment "OUTPUT_READABLE_EULER" if you want to see Euler angles
+// (in degrees) calculated from the quaternions coming from the FIFO.
+// Note that Euler angles suffer from gimbal lock (for more info, see
+// http://en.wikipedia.org/wiki/Gimbal_lock)
+#define OUTPUT_READABLE_EULER
+
+// uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
+// pitch/roll angles (in degrees) calculated from the quaternions coming
+// from the FIFO. Note this also requires gravity vector calculations.
+// Also note that yaw/pitch/roll angles suffer from gimbal lock (for
+// more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+// uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
+// components with gravity removed. This acceleration reference frame is
+// not compensated for orientation, so +X is always +X according to the
+// sensor, just without the effects of gravity. If you want acceleration
+// compensated for orientation, us OUTPUT_READABLE_WORLDACCEL instead.
+#define OUTPUT_READABLE_REALACCEL
+
+// uncomment "OUTPUT_READABLE_WORLDACCEL" if you want to see acceleration
+// components with gravity removed and adjusted for the world frame of
+// reference (yaw is relative to initial orientation, since no magnetometer
+// is present in this case). Could be quite handy in some cases.
+#define OUTPUT_READABLE_WORLDACCEL
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////
+///    SD    /////////////////////////////////
+/////////////////////////////////////////////
 
 File file;        // represents the log file on the SD-card
 
@@ -33,8 +93,15 @@ char fileName     [8];  // stores filename
 char folderName   [8];  // stores name of the folder
 
 
+////////////////////////////////////////////////
+///    CLOCK    ///////////////////////////////
+//////////////////////////////////////////////
+
 RTC_DS1307 RTC;
 RTC_Millis RTC_millis;
+
+
+
 
 void setup() {
 
@@ -58,7 +125,46 @@ void setup() {
 
     // verify connection
     Serial.println("Testing device connections...");
-    Serial.println( accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed" );
+    // [/] raw-data mode
+    // Serial.println( accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed" );
+    
+
+
+    ////////////////////////////////////////////////
+    ///    DMP-MODE    ////////////////////////////
+    //////////////////////////////////////////////
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(0, dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
+
 
     // configure Arduino LED for
     pinMode(LED_PIN, OUTPUT);
@@ -124,6 +230,8 @@ void setup() {
     
 }
 
+
+/*
 void loop() {
   
     // read raw accel/gyro measurements from device
@@ -169,4 +277,120 @@ void loop() {
     
     delay( LOOP_DELAY );
     
+}
+*/
+
+void loop() {
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        // other program behavior stuff here
+        // .
+        // .
+        // .
+        // if you are really paranoid you can frequently test in between other
+        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+        // while() loop to immediately process the MPU data
+        // .
+        // .
+        // .
+        
+        
+        DateTime now = RTC.now();
+        
+        // end of writing to file
+        file.flush(); // makes sure the information is written to the SD-card...
+        // ... without closing and re-opening the file
+        
+        delay( LOOP_DELAY );
+        
+        
+        // current timestamp from arduino millis() 
+        file.print( millis() ); file.print( "," );
+      
+        // current timestamp from the real time clock
+        file.print( now.unixtime() ); file.print( "," );
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+        
+
+        #ifdef OUTPUT_READABLE_EULER
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetEuler(euler, &q);
+            file.print(euler[0] * 180/M_PI);       file.print( "," );
+            file.print(euler[1] * 180/M_PI);       file.print( "," );
+            file.print(euler[2] * 180/M_PI);       file.print( "," );
+            
+        #endif
+
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            file.print(ypr[0] * 180/M_PI);       file.print( "," );
+            file.print(ypr[1] * 180/M_PI);       file.print( "," );
+            file.print(ypr[2] * 180/M_PI);       file.print( "," );
+        #endif
+
+        #ifdef OUTPUT_READABLE_REALACCEL
+            // display real acceleration, adjusted to remove gravity
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            
+            file.print(aaReal.x);               file.print( "," );
+            file.print(aaReal.y);               file.print( "," );
+            file.print(aaReal.z);           file.print( "," );
+            
+        #endif
+
+        #ifdef OUTPUT_READABLE_WORLDACCEL
+            // display initial world-frame acceleration, adjusted to remove gravity
+            // and rotated based on known orientation from quaternion
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+            
+            file.print(aaWorld.x);            file.print( "," );
+            file.print(aaWorld.y);            file.print( "," );
+            file.print(aaWorld.z);            file.print( "," );
+            
+        #endif
+        
+        file.print( "\n" );
+
+        // blink LED to indicate activity
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
+    }
 }
